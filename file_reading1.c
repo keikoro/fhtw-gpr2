897e6ed8d3cee4800a35f850c95b803f37a40b5c
linda23 <if13b076@technikum-wien.at>
2014-03-10 15:41:26 +0100

    can now read 4 letters from a file

--- file_reading1.c
+++ file_reading1.c
@@ -1,19 +1,17 @@
 /*
 A programm to practice read from and into a file.
-
 */
 
 #include <stdio.h>
 #include <string.h>
 
-
 int main()
 {
-    FILE *fp;
-    char c [10];
-    fp = fopen("testinput_auf1.txt", "r");
-    for (int i=0; (c[i] = getc(fp))!=EOF; i++)
-        c[i] = getc(fp);
-    printf("%s", c);
+    int n = 5;
+    FILE *F1;
+    char *line [10];
+    F1 = fopen("testinput_auf1.txt", "r");
+    fgets (line, n, F1);
+    printf("%s", line);
     return 0;
 }

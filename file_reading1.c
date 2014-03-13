a70017c83f91a324caa6493bc61f06c963a8b2f7
linda23 <if13b076@technikum-wien.at>
2014-03-13 15:39:30 +0100

    made a node structure, added namelength as an include, three text files with names

--- file_reading1.c
+++ file_reading1.c
@@ -3,14 +3,27 @@ Reads from file ue1_names.txt, prints the content on the screen.
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#define NAMENSLAENGE 256
+
+struct node{
+    char name[NAMENSLAENGE];
+    struct node *next;
+};
+
 
 int main()
 {
+
     FILE *F1;
-    char line [100];
+    char line [NAMENSLAENGE];
     F1 = fopen("ue1_names.txt", "r");
-    while (fgets (line, 25, F1))
+
+    struct node *head = NULL;
+
+    struct node *namenode;
+    while (fgets (line, NAMENSLAENGE, F1))
     {
         fputs(line,stdout);
     }

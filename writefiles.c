7057196341ebea36ff7567af5212293dac815ff6
K Kollmann <git@k.kollmann.moe>
2014-03-18 17:36:26 +0100

    changed variable names

--- writefiles.c
+++ writefiles.c
@@ -2,10 +2,10 @@
 write data into a file
 */
 
-int writefiles(char *outputfilename, struct node *traversenode)
+int writefiles(char *outputfilename, struct node *myoutputlist)
 {
     char *outputname = outputfilename;
-    struct node *outputlist = traversenode;
+    struct node *outputlist = myoutputlist;
 
     FILE *output;
     output = fopen(outputname, "w");

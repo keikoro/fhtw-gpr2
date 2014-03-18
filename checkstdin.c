eec6f9eca7b144a51d177ecfb6d1301e5e5efedc
K Kollmann <git@k.kollmann.moe>
2014-03-18 22:38:23 +0100

    new method: combine strings

--- checkstdin.c
+++ checkstdin.c
@@ -19,7 +19,8 @@ Return 1 in main() on any of these errors.
 
 
 TODO:
-- check if input file contains names (if not: print mgs, return 1)
+- check if input file contains names (if not: print mgs, *stopthis = 1;
+)
 -> happens in another function
 
 DONE:
@@ -27,17 +28,24 @@ DONE:
 attn: atm, only flags in front of a first file name are considered (unclear)
 e.g. -t -p name name name
 not: -t name name name -p
-- check for wrong flags (if there are: print msg, return 1)
-- check if input file exists (if not: print msg, return 1)
-- check if output file can be opened (if not: print msg, return 1)
+- check for wrong flags (if there are: print msg, *stopthis = 1;
+)
+- check if input file exists (if not: print msg, *stopthis = 1;
+)
+- check if output file can be opened (if not: print msg, *stopthis = 1;
+)
 */
 
-int checkstdin(int argc, char *argv[], char *namein, char *nameout, int *reverseflag)
+char *checkstdin(int argc, char *argv[], int *reverseflag, int *stopflag)
 {
     int getflags = 0;
     int *reverse = reverseflag;
-    char *inputfilename = namein;
-    char *outputfilename = nameout;
+    int *stopthis = stopflag;
+
+    char separator[1] = "|";
+    char *inputfilename;
+    char *outputfilename;
+    char *outputstring = (char*)malloc(256*sizeof(char));
     int i = 0, j = 0;
 
     FILE *input;
@@ -64,7 +72,8 @@ int checkstdin(int argc, char *argv[], char *namein, char *nameout, int *reverse
                 {
                     printf("sortnames: wrong option %#x\n", optopt);
                 }
-                return 1;
+                *stopthis = 1;
+                return outputstring;
                 break;
         }
     }
@@ -91,18 +100,16 @@ int checkstdin(int argc, char *argv[], char *namein, char *nameout, int *reverse
 
                 // blabalbalaaaaaa
                 // continue with program...
-
-                struct node *head;
-                head = read_and_sort(inputfilename);
-
-                struct node *myoutputlist;
-                myoutputlist=head;
-
-
                 if (*reverse == 1)
                 {
                     // sort list in reverse order
                 }
+
+                // guter outputstring
+
+                outputstring = strcat(inputfilename, separator);
+                outputstring = strcat(outputstring, outputfilename);
+                return outputstring;
 
                 fclose(input);
                 fclose(output);
@@ -111,20 +118,29 @@ int checkstdin(int argc, char *argv[], char *namein, char *nameout, int *reverse
             {
                 printf("sortnames: cannot open output file: %s\n", outputfilename);
                 fclose(input);
-                return 1;
+
+                *stopthis = 1;
+                outputstring = "\0";
+                return outputstring;
             }
         }
         else
         {
             printf("sortnames: cannot open input file: %s\n", inputfilename);
-            return 1;
+
+            *stopthis = 1;
+            outputstring = "\0";
+            return outputstring;
         }
     }
     /*  if there aren't enough filenames */
     else
     {
         printf("sortnames: wrong number of input or output files\n");
-        return 1;
+
+        *stopthis = 1;
+        outputstring = "\0";
+        return outputstring;
     }
-    return 0; // TODO check if this return is even necessary
+    *stopthis = 0;
 }

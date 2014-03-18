7fdbfb243faafcb6f9c89acb607a9d34842c473c
linda23 <if13b076@technikum-wien.at>
2014-03-18 19:20:25 +0100

    added function for reverse sorting

--- readandsort.c
+++ readandsort.c
@@ -175,3 +175,103 @@ struct node* read_and_sort(char *inputname)
     fclose(input);
     return (head);
 }
+
+
+struct node* read_and_sort_reverse(char *inputname)
+{
+    FILE *input;
+    char *filename = inputname;
+    struct node* head=NULL;
+    char current_line [NAMENSLAENGE];
+
+    struct node *to_be_inserted;
+    struct node *before_insertion;  // Will be the node after which the new node might get inserted
+    struct node *compared_with;     // Will be the node after which the new node might get inserted
+
+    input = fopen(filename, "r");
+
+
+    /*   reads each line, including the linebreak into the string*/
+    while (fgets(current_line, NAMENSLAENGE, input))
+    {
+        to_be_inserted=malloc(sizeof(struct node));
+
+        /*  copy the current line into a list node: */
+        strcpy(to_be_inserted->name, current_line);
+        /* Checking if line ends with a linebreak, add one if not */
+        if (to_be_inserted->name[strlen(to_be_inserted->name)-1] != '\n')
+        {
+            to_be_inserted->name[strlen(to_be_inserted->name)-1] = '\n';
+            to_be_inserted->name[strlen(to_be_inserted->name)] = '\0';
+        }
+
+        int wrong_input_format = 0;
+        for(int i=0; i<=strlen(to_be_inserted->name)-2; i++)
+        /* Check for wrong input format;
+            Character is neither A-Z, nor a-z, nor a dash, nor a space
+        */
+        {
+            if(
+               !(
+                (   (to_be_inserted->name[i] >= 'A') && (to_be_inserted->name[i] <= 'Z') )
+                    ||
+                (   (to_be_inserted->name[i] >= 'a') && (to_be_inserted->name[i] <= 'z') )
+                    ||
+                    ( isspace(to_be_inserted->name[i]) )
+                    ||
+                    (to_be_inserted->name[i] == '-')
+                )
+            )
+                wrong_input_format = 1;
+        }
+
+    if (wrong_input_format == 1)
+    /*Needs to end the whole programm, not just this function as it does now.*/
+
+    {
+        printf("sortnames: wrong input format\n");
+        head=NULL;
+        return(head);
+    }
+    /*Find right spot to insert the new node into: */
+        if (head == NULL)
+        {
+            head=to_be_inserted;
+        }
+        else
+        {
+            if(compare_strings(head->name, to_be_inserted->name)==1)
+            {
+                to_be_inserted->next=head;
+                head=to_be_inserted;
+            }
+            else
+            {
+                before_insertion=head;
+                compared_with=head->next;
+                while (compared_with != NULL)
+                {
+                    if(compare_strings(to_be_inserted->name,compared_with->name)==2)
+                    {
+                        before_insertion->next=to_be_inserted;
+                        to_be_inserted->next=compared_with;
+                        break;
+                    }
+                    else
+                    {
+                        before_insertion=before_insertion->next;
+                        compared_with=compared_with->next;
+                    }
+                }
+                if (compared_with==NULL)
+                /* If the new node has to be placed at the end of the current list
+                */
+                {
+                    before_insertion->next=to_be_inserted;
+                }
+            }
+        }
+    }
+    fclose(input);
+    return (head);
+}

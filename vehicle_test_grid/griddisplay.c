1d701cdeb1d1dd082b1b297ddb50e6b85a54ad88
K Kollmann <git@k.kollmann.moe>
2014-04-28 10:44:42 +0200

    added authors, basic main

---
+++ vehicle_test_grid/griddisplay.c
@@ -0,0 +1,15 @@
+/*  vehicle_grid_server program
+    griddisplay.c
+
+    GPR2 UE4
+
+    if13b070 - K Kollmann
+    if13b076 - Linda Spindler
+*/
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}

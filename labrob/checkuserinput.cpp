422e30a12de968aede41bfde9ce9513813209809
K Kollmann <git@k.kollmann.moe>
2014-05-13 20:34:18 +0200

    changes to getopt check

--- labrob/checkuserinput.cpp
+++ labrob/checkuserinput.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <getopt.h>
+#include <stdlib.h>     /* exit, EXIT_FAILURE */
 #include "mazes.h"
 #include "robots.h"
 
@@ -10,45 +11,46 @@
 void checkuserinput (int argc, char *argv[]) {
 
     // std::ifstream mazefile (argv[1], std::ifstream::in);
-    int c;
-    int error = 0;
+    int err = 0;
     int i;
-    char *robottype=NULL;
-    char *usagemsg=NULL;
+    int robottype = 0;
+    // int usagemsg = 0;
     char *programname=argv[0];
+    int getflags = 0;
     // char *filename=argv[1];
 
-    while((c = getopt(argc, argv, "th")) != EOF)
+
+    while ((getflags = getopt(argc, argv, "th")) != -1)
     {
-        switch(c)
+        /*  suppress auto system error msgs on wrong flags */
+        //opterr = 0;
+        switch (getflags)
         {
             case 't':
-                if (robottype!=NULL) /* Argument used more than once */
-                {
-                  error=1;
-                  break;
-                }
-                robottype=optarg;
-                printf("Using a robot: %s\n", robottype);
+                robottype = optopt;
+                printf("Using a robot: %c\n", robottype);
                 break;
             case 'h':
-                if (usagemsg!=NULL)
-                {
-                  error=1;
-                  break;
-                }
-                usagemsg=optarg;
-                // following printf to be deleted later, but useful for debugging
+                // usagemsg = optopt;
                 printf("To send a robot through the labyrinth, \nplease "
                         "enter the filename of the maze you'd like to use\n"
                         "as well as the shortcut of the robot (t1 to t3), "
                         "like so:\n"
                         "%s filename_of_maze -t1\n", programname);
                 break;
+            /*  if unknown flags were given */
             case '?':
-                error=1;
-                break;
-            default:
+                /*  print out printable flags */
+                if (isprint(optopt))
+                {
+                    fprintf(stderr, "%s: wrong option %c\n", argv[0], optopt);
+                }
+                /*  for non-printable characters */
+                else
+                {
+                    fprintf(stderr, "%s: wrong option %c\n", argv[0], optopt);
+                }
+                exit(1);
                 break;
         }
     }
@@ -60,5 +62,10 @@ void checkuserinput (int argc, char *argv[]) {
          printf("Argument #%d is: %s\n", i, argv[i]);
     }
 
+    if(err == 1)
+    {
+        printf("You provided a wrong number of arguments!\n");
+    }
+    
 }
 

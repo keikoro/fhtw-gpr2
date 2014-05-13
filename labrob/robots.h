fb40c0830318894733596e33fdab69d34e4ed759
K Kollmann <git@k.kollmann.moe>
2014-05-13 23:29:25 +0200

    comment student data for home excercise, formatting

--- labrob/robots.h
+++ labrob/robots.h
@@ -1,9 +1,18 @@
+/*  labrob program
+    robots.h
+
+    GPR2 UE5 (+ UE6, UE7)
+
+    if13b070 - K Kollmann
+    if13b076 - Linda Spindler
+
+*/
+
 #ifndef ROBOTS_H
 #define ROBOTS_H
 
-
-class robots{
-public:	
+class robots {
+public:
 	//~ robots();
 	//~ ~robots();
 	char robotcharacter;	// character the robot draws on the labyrinth,

2b5787aacb289d6b40c8bafc32ae3d4d0b6e28cc
K Kollmann <git@k.kollmann.moe>
2014-05-13 18:46:57 +0200

    add ausgelagerte files

---
+++ labrob/checkuserinput.cpp
@@ -0,0 +1,44 @@
+
+void checkuserinput (int argc, char *argv[]) {
+
+
+//     std::ifstream mazefile (argv[1], std::ifstream::in);
+//     int c;
+//     int error=0;
+//     char *robottype=NULL;
+//     char *usagemsg=NULL;
+//     char *programname=argv[0];
+//     char *filename=argv[1];
+
+//     while((c = getopt(argc, argv, "th")) != EOF)
+//     {
+//         switch(c)
+//         {
+//             case 't':
+//                 if (robottype!=NULL) /* Argument used more than once */
+//                 {
+//                   error=1;
+//                   break;
+//                 }
+//                 x_axis=optarg;
+//                 printf("%s: parsing option x, argument: %s\n", programname, x_axis);
+//                 break;
+//             case 'h':
+//                 if (usagemessage!=NULL)
+//                 {
+//                   error=1;
+//                   break;
+//                 }
+//                 y_axis=optarg;
+//                 // following printf to be deleted later, but useful for debugging
+//                 printf("Use the program like this: labrob DATEINAME [-t1] [–t2] [–t3]...[-tN] [-h]", y_axis);
+//                 break;
+//             case '?':
+//                 error=1;
+//                 break;
+//             default:
+//                 break;
+//         }
+//     }
+}
+
---
+++ labrob/classes.h
@@ -0,0 +1,10 @@
+
+class mazes {
+public:
+    std::string one_row;
+    std::vector<std::string> v_maze;
+    void checkuserinput(int, char**, char**);
+    void createmaze();
+    std::vector<std::string> lindacreatemaze();
+};
+

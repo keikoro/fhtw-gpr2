4fcfe0964f8a535b0042bb6bcdba391306221d98
linda <if13b076@technikum-wien.at>
2014-05-13 18:51:52 +0200

    removed all unused parts of mazes class (all but the maze vector)

--- labrob/classes.h
+++ labrob/classes.h
@@ -1,10 +1,4 @@
-
 class mazes {
 public:
-    std::string one_row;
     std::vector<std::string> v_maze;
-    void checkuserinput(int, char**, char**);
-    void createmaze();
-    std::vector<std::string> lindacreatemaze();
 };
-

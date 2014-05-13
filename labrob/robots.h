e9952097053cd8951bdb1c0ea855748977cb00aa
linda <if13b076@technikum-wien.at>
2014-05-13 18:58:26 +0200

    made a h file for the robots class, is almost empty

---
+++ labrob/robots.h
@@ -0,0 +1,7 @@
+class robots{
+public:	
+	char robotcharacter;	// character the robot draws on the labyrinth,
+							// has to be accessible from outside
+private:
+	void exit_search(); // function that will contain the search algorithm
+}

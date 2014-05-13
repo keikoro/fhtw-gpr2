ddfbc78cfca3b1c1a8c1efc0183f6eafea640733
K Kollmann <git@k.kollmann.moe>
2014-05-13 18:47:19 +0200

    move stuff out of labrob

--- labrob/labrob.cpp
+++ labrob/labrob.cpp
@@ -15,22 +15,15 @@
 #include <string>
 #include <vector>
 #include <getopt.h>
+#include "classes.h"
+#include "checkuserinput.cpp"
 
 using namespace std;
 
-// TODO: remove later, überbleibsel of original function
-// void checkuserinput(int argc, char *argv[], char **inputfile);
+void checkuserinput (int argc, char argv[]);
 
-//lindacreatemaze/createmaze are not in use
 
-class mazes {
-public:
-    string one_row;
-    vector<string> v_maze;
-    void checkuserinput(int, char**, char**);
-    void createmaze();
-    std::vector<string> lindacreatemaze();
-};
+//  lindacreatemaze/createmaze are not in use
 std::vector<string> lindacreatemaze (char** inputfile)
 {
     string one_row;
@@ -75,62 +68,12 @@ int main(int argc, char *argv[])
 
     mazes mymaze; //make an instance of mazeses called mymaze
 	mymaze.v_maze = v_maze; //the v_maze vector is equal toe the vector v_maze here in main
-    
+
+    checkuserinput(argc, argv);
+
     cout << "Our current maze:\n";
     for(unsigned int i=0; i<mymaze.v_maze.size(); i++)
         cout << mymaze.v_maze[i] << endl;
 
     return 0;
 }
-
-void mazes::checkuserinput (int argc, char *argv[], char **inputfile) {
-
-    // function that includes/refers to checkuserinput (= getopt func)
-    // ... ??
-
-    // std::ifstream mazefile (argv[1], std::ifstream::in);
-
-
-    // getopt blabla...
-    // copy-pasted from gridserver program
-    // needs adaption....
-
-    // int c;
-    // int error=0;
-    // char *x_axis=NULL;
-    // char *y_axis=NULL;
-    // program_name=argv[0];
-
-    // while( (c = getopt(argc, argv, "x:y:")) != EOF )
-    // {
-    //     switch( c )
-    //     {
-    //         case 'x':
-    //             if (x_axis!=NULL) /* Argument used more than once */
-    //             {
-    //               error=1;
-    //               break;
-    //             }
-    //             x_axis=optarg;
-    //             printf("%s: parsing option x, argument: %s\n",inputfile,x_axis);
-    //             break;
-    //         case 'y':
-    //             if (y_axis!=NULL)
-    //             {
-    //               error=1;
-    //               break;
-    //             }
-    //             y_axis=optarg;
-    //             // following printf to be deleted later, but useful for debugging
-    //             printf("%s: parsing option y, argument: %s\n",inputfile,y_axis);
-    //             break;
-    //         case '?':
-    //             error=1;
-    //             break;
-    //         default:
-    //             assert( 0 );
-    //     }
-    // }
-
-}
-

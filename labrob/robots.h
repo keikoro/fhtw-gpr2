b7928961ae3ca91764f93857a9400ef7397f0bdf
linda <if13b076@technikum-wien.at>
2014-05-13 21:58:45 +0200

    added coordinate ints to the class

--- labrob/robots.h
+++ labrob/robots.h
@@ -7,13 +7,10 @@ public:
 	//~ robots();
 	//~ ~robots();
 	char robotcharacter;	// character the robot draws on the labyrinth,
-							// has to be accessible from outside
+	int x,y;				// integers for saving the
+							// coordinates of the robot
 protected:
 	void exit_search(); // function that will contain the search algorithm
 };
-// dummy function for exit search
-//~ void exit_search(){
-	//~ if (mazes.v_maze[0][0] == '#')
-		//~ mazes.v_maze[0][0] = 'a';
-//~ };
+
 #endif // ROBOTS_H

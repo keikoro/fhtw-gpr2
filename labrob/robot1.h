0192d5d94032d9f2c17d6b27eb9186fefe11de4b
K Kollmann <git@k.kollmann.moe>
2014-05-13 23:32:17 +0200

    comment student data for home excercise, formatting

--- labrob/robot1.h
+++ labrob/robot1.h
@@ -1,13 +1,23 @@
-// subclass of "robots": for robots of type 1
+/*  labrob program
+    robot1.h
 
-#ifndef T1_H
-#define T1_H
+    GPR2 UE5 (+ UE6, UE7)
+
+    if13b070 - K Kollmann
+    if13b076 - Linda Spindler
+
+    subclass of "robots": for robots of type 1
+
+*/
+
+#ifndef ROBOT1_H
+#define ROBOT1_H
 
 class t1 : public robot {
 public:
 
 protected:
-	void t1_exit_search();
+    void t1_exit_search();
 };
 
 #endif // T1_H

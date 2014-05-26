e71735499aa4ed3c2a01dfceea37c715c99f196f
linda <if13b076@technikum-wien.at>
2014-05-26 20:37:04 +0200

    bottom line still doesn't work

--- labrob/mazes_findentrance.cpp
+++ labrob/mazes_findentrance.cpp
@@ -22,6 +22,8 @@ int Mazes::find_entrance(std::vector<std::string> v_maze)
 	bool found_entrance = false;
 	bool found_exit = false;
 
+// cout << "Length: " << maze_length << endl; << "Height: " << maze_height <<endl;
+
 	// look for entrance/exit at the top; the corner is irrelavant
 	// so stop at maze_length - 2
 	for (unsigned int i = 1; i < maze_length-1; ++i)
@@ -48,21 +50,21 @@ int Mazes::find_entrance(std::vector<std::string> v_maze)
 	// if the entrance/exit is not in the first row look on the right side
 	if (found_entrance == false || found_exit == false)
 	{
-		for (unsigned int i=1; i < maze_height-2; ++i)
+		for (unsigned int i=1; i <= maze_height-2; ++i)
 		{
 			if(v_maze[i][maze_length-1] != '#')
 			{
 				if (found_entrance == false)
 				{
 					found_entrance = true;
-					entrance[0] = v_maze[0].length();
+					entrance[0] = v_maze[0].length()-1;
 					entrance[1] = i;
 					continue; // next iteration of the loop
 				}
 					else
 				{
 					(found_exit = true);
-					mazeexit[0] = v_maze[0].length();
+					mazeexit[0] = v_maze[0].length()-1;
 					mazeexit[1] = i;
 					return 0;
 				}
@@ -73,7 +75,7 @@ int Mazes::find_entrance(std::vector<std::string> v_maze)
 	// look for entrance/exit at the bottom
 	if (found_entrance == false || found_exit == false)
 	{
-		for (unsigned int i = maze_length-1 ; i >= 1 ; --i)
+		for (unsigned int i = maze_length-2 ; i >= 1 ; --i)
 		{
 			if(v_maze[maze_height-2][i] != '#')
 			{
@@ -81,14 +83,14 @@ int Mazes::find_entrance(std::vector<std::string> v_maze)
 				{
 					found_entrance = true;
 					entrance[0] = i;
-					entrance[1] = maze_height;
+					entrance[1] = maze_height-1;
 					continue; // next iteration of the loop
 				}
 					else
 				{
 					(found_exit = true);
 					mazeexit[0] = i;
-					mazeexit[1] = maze_height;
+					mazeexit[1] = maze_height-1;
 					return 0;
 				}
 			}

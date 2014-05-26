8a0c19cb1e4022ade9adb1d860c64698cc128614
linda <if13b076@technikum-wien.at>
2014-05-26 19:47:58 +0200

    moved functions to separate files

---
+++ labrob/mazes_dummyprint.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+
+
+// function that prints the exit and entrance coordinates for debugging
+void Mazes::dummy_function_for_printing()
+{
+	cout << "Coordinates of the entrance: X, Y: " <<
+		entrance[0] << "," << entrance[1] << endl;
+
+	cout << "Coordinates of the exit: X, Y: " <<
+		mazeexit[0] << "," << mazeexit [1] << endl;
+
+}

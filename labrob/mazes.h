9934a3f3c6811742bd13decdbfb2fb1278709514
K Kollmann <git@k.kollmann.moe>
2014-05-27 17:43:24 +0200

    improved comments/program description, fixed formatting

--- labrob/mazes.h
+++ labrob/mazes.h
@@ -8,13 +8,14 @@
 
     class for mazes
 
-	IMPORTANT: the maze is saved as a string vector.
-	This means that when referencing one character withing the maze like this:
-		vector[v][h] (where "vector" is the vector and v and h are ints)
-	"v" is the index on the y-axis (vertical), and 
+	IMPORTANT:
+	The maze is saved as a string vector, which means that when one
+	wants to reference a character within the maze like so:
+		vector[v][h]
+	(where "vector" is the vector and v and h are integers)
+	"v" is the index on the y-axis (vertical) and
 	"h" the index on the x-axix (horizontal)
 */
-
 
 #ifndef MAZES_H
 #define MAZES_H
@@ -28,10 +29,10 @@ public:
 //private:
 	// Vector that contains the maze
 	std::vector<std::string> v_maze;
-	
-	/* int arrays for saving the coordinated of entrance and exit of
+
+	/*	int arrays for saving the coordinates of entrance and exit of
 		the maze, and method for finding the entrance as well as the
-		robot start position in the robot; char for saving
+		robot's start position in the robot; use a char for saving
 		that start position
 	*/
 	int entrance [2];
@@ -40,19 +41,19 @@ public:
 	char startposition;
 
 	void dummy_function_for_printing();
-	
-	// A vector that saves pointers to instances of robots
+
+	// a vector that saves pointers to instances of robots
 	std::vector<Robots*> robot_list;
-	
+
 	// function for adding new robots to the vector
 	void add_robot (Robots *a_robot, Mazes maze);
-	
-	// function for printing out the list of all robots
-	void print_robots(Mazes mymaze);	
-	
-	/* function to check whether the char right in front of the 
-	   robot is a wall or not */
-	bool is_wall(int v, int h, char direction, 
+
+	// function for printing out a list of all robots
+	void print_robots(Mazes mymaze);
+
+	/*	function to check whether the char right in front of the
+		robot is a wall or not */
+	bool is_wall(int v, int h, char direction,
 		std::vector<std::string> v_maze);
 };
 

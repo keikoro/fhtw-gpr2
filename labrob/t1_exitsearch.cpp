ed725ab3537dc0c13b51b166c72734e8938cd92f
K Kollmann <git@k.kollmann.moe>
2014-05-27 19:12:08 +0200

    function for robot t1 to find through the labyrinth

---
+++ labrob/t1_exitsearch.cpp
@@ -0,0 +1,16 @@
+/*  labrob program
+    t2_exitsearch.cpp
+
+    GPR2 UE5 + UE6 + UE7
+    if13b070 - K Kollmann
+    if13b076 - Linda Spindler
+
+    Search function for robots of type 1.
+    Type 1 robots try to find their way through the maze
+    by always turning right.
+*/
+
+void t1::exit_search(Robots thisrobot, Mazes thismaze)
+{
+    step_counter = 0;
+}

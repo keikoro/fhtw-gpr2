573b5bcc0bb4290931b42b8fb9116b1ff4a83a75
K Kollmann <git@k.kollmann.moe>
2014-05-27 17:07:19 +0200

    move t2::exit_search out of labrob.cpp for collab

--- labrob/labrob.cpp
+++ labrob/labrob.cpp
@@ -83,7 +83,8 @@
 #include "checkuserinput.cpp"
 #include "mazes_findentrance.cpp"
 #include "mazes_dummyprint.cpp"
-
+#include "mazes_iswall.cpp"
+#include "t2_exitsearch.cpp"
 
 using namespace std;
 
@@ -243,37 +244,6 @@ Robots Robots::step_forward(Robots robot)
 
 	return (robot);
 }
-
-
-
-
-void t2::exit_search(Robots robot, Mazes this_maze)
-{
-	step_counter = 0;
-
-	/* coordinated of the robot, are directly accessible since they're
-		stored in Robots (or t2)
-
-	*/
-	while (! ( (robot.v == this_maze.mazeexit[0]) && (robot.h == this_maze.mazeexit[1]) ) )
-	{
-		cout << robot.v << robot.h << endl;
-		if (this_maze.is_wall(robot.v, robot.h, robot.direction, this_maze.v_maze))
-		{
-			// if there's a wall turn left and try again
-			robot.direction = turn_left(robot);
-			exit_search(robot, this_maze);
-		}
-		else
-		{
-			robot = step_forward(robot);
-			step_counter++;
-		}
-	}
-	cout << "Steps taken: " << step_counter << endl;
-	cout << "Current position (X,Y): " << robot.h << ", " << robot.v << endl;
-}
-
 
 void Robots::exit_search(Robots robot, Mazes this_maze)
 {

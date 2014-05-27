e4ed6dd8dfb79caa7e703b919b89703813e0dea7
K Kollmann <git@k.kollmann.moe>
2014-05-27 19:47:50 +0200

    new version of step forward

---
+++ labrob/robots_stepforward_v2.cpp
@@ -0,0 +1,38 @@
+/*  labrob program
+    robots_stepforward.cpp
+
+    GPR2 UE5 + UE6 + UE7
+
+    if13b070 - K Kollmann
+    if13b076 - Linda Spindler
+
+    function that makes a robot step forward
+    (direction in which it steps is dependend on direction it is facing)
+*/
+
+Robots Robots::step_forward_v2(Robots robot)
+{
+    if (robot.direction == 'n')
+    {
+        robot.v--;
+    }
+    else
+    if (robot.direction == 'w')
+    {
+        robot.h--;
+    }
+    else
+    if (robot.direction == 's')
+    {
+        robot.v++;
+    }
+    else
+    if (robot.direction == 'e')
+    {
+        robot.v++;
+    }
+    /*  message for debugging -- TODO: remove later on */
+    else cout << "something went wrong (func Robots::step_forward)" << endl;
+
+    return (robot);
+}

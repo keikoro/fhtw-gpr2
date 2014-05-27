5191d3f0248c94ced5ca44925cd7cb579de35161
linda <if13b076@technikum-wien.at>
2014-05-28 00:00:05 +0200

    one file with everything pasted in

--- labrob/labrob.cpp
+++ labrob/labrob.cpp
@@ -19,21 +19,7 @@
     -h being used for printing a usage message
 
 
-    TODOS / ISSUES:
-    - need to free the allocated space again, right now program
-    doesn't do this at all
-    - exit_search function in t2 isn't working; need to pay attention to
-    what the recursion actually does
-    - made everything in the classes public, remember to put everything
-    that doesn't need to be public back to private later, but only after
-    we're done with the whole program
-    - maybe also use "const" (which can be used for making something in
-    a class visible but read-only from outside)
-    - for efficiency's sake, maybe find entrance and exit right when
-    reading in the file?
-	- add_robot needing a Mazes argument is a bit awkward; works tho
 */
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -46,23 +32,11 @@
 #include "mazes.h"
 #include "t1.h"
 #include "t2.h"
-#include "checkuserinput.cpp"
-#include "mazes_findentrance.cpp"
-#include "mazes_dummyprint.cpp"
-#include "mazes_iswall.cpp"
-#include "robots_stepforward.cpp"
-#include "robots_stepforward_v2.cpp"
-#include "robots_turnleft.cpp"
-#include "t2_exitsearch.cpp"
-#include "t1_exitsearch.cpp"
 
 using namespace std;
 
-// function declarations
-/*  currently commented out as they are in separate files;
-    uncomment again later on/add proper declarations -- TODO */
-// void checkuserinput(int argc, char *argv[], string *mazefile,
-//                     vector<int> *robot_numbers);
+ void checkuserinput(int argc, char *argv[], string *mazefile,
+                     vector<int> *robot_numbers);
 
 int main(int argc, char *argv[])
 {
@@ -136,7 +110,7 @@ int main(int argc, char *argv[])
         cout << mymaze.v_maze[i] << endl;
     }
 
-	mymaze.dummy_function_for_printing();
+//	mymaze.dummy_function_for_printing();
     return 0;
 }
 
@@ -168,14 +142,482 @@ void Robots::exit_search(Robots robot, Mazes this_maze)
 	cout << "This robot is looking for an exit" << endl;
 }
 
-
-/*  how to free/delete vectors??
-    https://stackoverflow.com/questions/
-    10464992/c-delete-vector-objects-free-memory
+/*    function that makes a robot step forward
+    (direction in which it steps is dependend on direction it is facing)
 */
 
-// delete vector<Robots*>;
-// vector<string> v_maze;
-// vector<int> robot_numbers;
-// vector<tempObject>().swap(tempVector);
-// vector<string> v_maze().swap(v_maze);
+Robots Robots::step_forward(Robots robot)
+{
+    if (robot.direction == 'n')
+    {
+        robot.v--;
+    }
+    else
+    if (robot.direction == 'w')
+    {
+        robot.h--;
+    }
+    else
+    if (robot.direction == 's')
+    {
+        robot.v++;
+    }
+    else
+    if (robot.direction == 'e')
+    {
+        robot.h++;
+    }
+    /*  message for debugging -- TODO: remove later on */
+    else cout << "something went wrong (func Robots::step_forward)" << endl;
+
+    return (robot);
+}
+
+/*
+    function that makes a robot turn to its left
+*/
+
+char Robots::turn_left(Robots robot)
+{
+	switch(robot.direction)
+	{
+		case 'n': return ('w');
+		break;
+		case 'w': return ('s');
+		break;
+		case 's': return ('e');
+		break;
+		case 'e': return ('n');
+		break;
+	}
+return 'x';
+}
+
+
+
+char Robots::turn_right(Robots robot)
+{
+	switch(robot.direction)
+	{
+		case 'n': return ('w');
+		break;
+		case 'w': return ('n');
+		break;
+		case 's': return ('w');
+		break;
+		case 'e': return ('s');
+		break;
+	}
+return 'x';
+}
+
+/*
+    Search function for robots of type 2 (called when user enters -t2
+    as an option). Takes an instance of Robots and an instance of Mazes as
+    arguments and calls the functions Mazes::is_wall, Robots::turn_left
+    and Robots::step_forward.
+
+    The robot checks if there is a wall in front of it. If there is,
+    it turns left and recursively calls the function again. If there isn't,
+    it moves one step forward. The robot also saves the number of steps
+    it has already taken.
+*/
+
+
+void t2::exit_search(Robots robot, Mazes this_maze)
+{
+    int step_count = 0;
+    /*  coordinates of the robot are directly accessible since they're
+        stored in Robots (or t2)
+    */
+    
+//    bool stop = false;
+    
+    int t = 23;
+	
+	while (t > 0)
+	{
+		/*
+			As long as there is no wall in front and a wall on the left,
+			keep going; also keep going if this was the very first step
+			because then we treat the entrance as a wall
+		*/
+		if(
+			(!(this_maze.is_wall(robot.v, robot.h, robot.direction, this_maze.v_maze)) )
+			&&
+			( (this_maze.wall_left(robot.v, robot.h, robot.direction, this_maze.v_maze))
+					||
+			  (step_count == 1)
+		    )
+		   )
+		{
+			robot = step_forward(robot);
+			step_count++;
+			cout << "V: " << robot.v << ", H: " << robot.h << endl;
+		}
+		else
+		{
+			cout << robot.direction << endl;
+			// if there's a wall on the left, turn right
+			if (this_maze.wall_left(robot.v, robot.h, robot.direction, this_maze.v_maze))
+			{
+				robot.direction = turn_right(robot);
+				cout << "V again: " << robot.v << ", H again: " << robot.h << endl;
+
+			}
+			// if there is no wall on the left, turn left
+			else
+			{
+				robot.direction = turn_left(robot);
+			}
+			// in both cases, leave the loop
+		}
+		t--;
+	}
+	cout << "Steps: " << step_count << endl;
+}
+
+/*
+    Search function for robots of type 1.
+    Type 1 robots try to find their way through the maze
+    by always turning right.
+*/
+
+void t1::exit_search(Robots thisrobot, Mazes thismaze)
+{
+    thisrobot.step_counter = 0;
+
+    // thisrobot.h // x coordinate;
+    // thisrobot.v // y coordinate;
+    // thisrobot.direction // direction the robot is currently facing;
+
+    cout << endl << "X: " << thisrobot.h << " Y: " << thisrobot.v << endl << endl
+    << "facing: " << thisrobot.direction << endl << endl;
+
+    thisrobot = step_forward(thisrobot);
+    thisrobot.step_counter++;
+
+    cout << endl << "X: " << thisrobot.h << " Y: " << thisrobot.v << endl << endl
+    << "facing: " << thisrobot.direction << endl << endl;
+
+    thisrobot = step_forward(thisrobot);
+    thisrobot.step_counter++;
+
+    cout << endl << "X: " << thisrobot.h << " Y: " << thisrobot.v << endl << endl
+    << "facing: " << thisrobot.direction << endl << endl;
+
+    cout << "steps: " << thisrobot.step_counter << endl << endl;
+
+}
+
+/*
+    Method in mazes that checks whether the robot has a wall in front of
+    itself (what "in front of" mean depends on which way the robot is
+    facing (=char direction). Returns TRUE if there is a wall and FALSE
+    is there isn't
+*/
+
+using namespace std;
+
+bool Mazes::is_wall(int v, int h, char direction,
+            vector<string> v_maze)
+{
+    if (direction == 'n')
+    {
+        if (v_maze[v-1][h] == '#')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    if (direction == 'w')
+    {
+        if (v_maze[v][h-1] == '#')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    if (direction == 's')
+    {
+        if (v_maze[v+1][h] == '#')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    // (direction == 'e')
+    {
+        if (v_maze[v][h+1] == '#')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+
+bool Mazes::wall_left(int v, int h, char direction,
+            vector<string> v_maze)
+{
+    if (direction == 'n')
+    {
+        if (v_maze[v][h-1] == '#')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    if (direction == 'w')
+    {
+        if (v_maze[v+1][h] == '#')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    if (direction == 's')
+    {
+        if (v_maze[v][h+1] == '#')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    // (direction == 'e')
+    {
+        if (v_maze[v-1][h] == '#')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+int Mazes::find_entrance(std::vector<std::string> v_maze)
+{
+	 // length of the maze is length of the strings:
+	unsigned int maze_length = v_maze[0].length();
+
+	// height of the maze is length of the vector:
+	unsigned int maze_height = v_maze.size();
+	cout << maze_height << endl;
+
+	bool found_entrance = false;
+	bool found_exit = false;
+
+// cout << "Length: " << maze_length << endl; << "Height: " << maze_height <<endl;
+
+	/*	look for entrance/exit in the top row of the maze;
+		the corner is irrelevant, so stop at maze_length-2 */
+	for (unsigned int i = 1; i < maze_length-1; ++i)
+	{
+		if(v_maze[0][i] != '#')
+		{
+			if (found_entrance == false)
+			{
+				found_entrance = true;
+				entrance[0] = 0;
+				entrance[1] = i;
+				startposition = 's';
+				continue; // next iteration of the loop
+			}
+				else
+			{
+				found_exit = true;
+				mazeexit[0] = 0;
+				mazeexit[1] = i;
+//				cout << "Exit is at the top" << endl;
+				return 0;
+			}
+		}
+	}
+
+	/*	if the entrance/exit is not in the first row,
+		look on the right side of the maze */
+	if (found_entrance == false || found_exit == false)
+	{
+		for (unsigned int i=1; i < maze_height-1; ++i)
+		{
+			if(v_maze[i][maze_length-1] != '#')
+			{
+				if (found_entrance == false)
+				{
+					found_entrance = true;
+					entrance[0] = i;
+					entrance[1] = maze_length-1;
+					startposition = 'w';
+					continue; // next iteration of the loop
+				}
+					else
+				{
+					found_exit = true;
+					mazeexit[0] = i;
+					mazeexit[1] = maze_length-1;
+		//			cout << "Exit is on the right" << endl;
+					return 0;
+				}
+			}
+		}
+	}
+
+	/*	look for entrance/exit at the bottom */
+	if (found_entrance == false || found_exit == false)
+	{
+		for (unsigned int i = maze_length-2 ; i >= 1 ; --i)
+		{
+			if(v_maze[maze_height-1][i] != '#')
+			{
+				if (found_entrance == false)
+				{
+					found_entrance = true;
+					entrance[0] = maze_height-1;
+					entrance[1] = i;
+					startposition = 'n';
+					continue; // next iteration of the loop
+				}
+					else
+				{
+					found_exit = true;
+					mazeexit[0] = maze_height-1;
+					mazeexit[1] = i;
+	//				cout << "Exit is at the bottom" << endl;
+					return 0;
+				}
+			}
+		}
+	}
+
+	/*	look for entrance on the left */
+	if (found_entrance == false || found_exit == false)
+	{
+		for (unsigned int i=maze_height-2; i >= 1 ; --i)
+		{
+			if(v_maze[i][0] != '#')
+			{
+				if (found_entrance == false)
+				{
+					found_entrance = true;
+					entrance[0] = i;
+					entrance[1] = 0;
+					startposition = 'e';
+					continue; // next iteration of the loop
+				}
+					else
+				{
+					found_exit = true;
+					mazeexit[0] = i;
+					mazeexit[1] = 0;
+					return 0;
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+
+void checkuserinput (int argc, char *argv[], string *mazefile, vector<int> *robot_numbers) {
+
+    string helpmsg = "Usage: labrob path_to_maze "
+                        "[-t1] [–t2] [–t3]...[-tN] [-h]";
+    int getflags;
+    int thisrobot;
+    vector<int> initialrobotnumbers;
+    string placeholder = "";
+    *mazefile = placeholder;
+    // string *maze = NULL;
+
+    /*  use double colon for option argument for the*/
+    while ((getflags = getopt(argc, (char **)argv, "t::h")) != -1) {
+
+        switch(getflags) {
+            case 'h':
+                cout << helpmsg << endl;
+                exit(EXIT_FAILURE);
+                break;
+            case 't':
+                /*  if no argument is given, use 1  */
+                if (optarg == NULL)
+                {
+                    /*  check if argument is a number  */
+                    thisrobot = 1;
+                    robot_numbers->push_back(thisrobot);
+                }
+                else
+                {
+                    if (isdigit(*optarg))
+                    {
+                        sscanf(optarg, "%d", &thisrobot);
+                        robot_numbers->push_back(thisrobot);
+                        /* below for debugging -- TODO: remove later on */
+                        // cout << "robot " << thisrobot << endl;
+                    }
+                    else
+                    {
+                        cout << "invalid argument: " << optarg << endl;
+                        cout << helpmsg << endl;
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                break;
+            /*  unknown option used */
+            case '?':
+                cout << "invalid option: " << optarg << endl
+                << helpmsg << endl;
+                exit(EXIT_FAILURE);
+                break;
+        }
+    }
+
+    /*  there must be at least 3 argv (program name, -t, file name of maze) */
+    if (sizeof(argv) < 3)
+    {
+        cout << "not enough options provided" << endl << helpmsg << endl;
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        if (argv[optind])
+        {
+            /*  store the name of the maze file
+                attn.: validity of file name is not yet confirmed */
+            *mazefile = string(argv[optind]);
+        }
+        else
+        {
+            cout << "no maze file was provided!" << endl << helpmsg << endl;
+        }
+    }
+}
+

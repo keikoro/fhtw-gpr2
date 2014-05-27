59f51e82a31d26ab4dfa48cbbd0a3c6f81e82979
K Kollmann <git@k.kollmann.moe>
2014-05-27 17:46:50 +0200

    fixed spelling & formatting in comments

--- labrob/t2_exitsearch.cpp
+++ labrob/t2_exitsearch.cpp
@@ -19,7 +19,7 @@
 void t2::exit_search(Robots robot, Mazes this_maze)
 {
     step_counter = 0;
-    /*  coordinated of the robot are directly accessible since they're
+    /*  coordinates of the robot are directly accessible since they're
         stored in Robots (or t2)
     */
     while (! ( (robot.v == this_maze.mazeexit[0]) && (robot.h == this_maze.mazeexit[1]) ) )
@@ -27,7 +27,7 @@ void t2::exit_search(Robots robot, Mazes this_maze)
         cout << robot.v << robot.h << endl;
         if (this_maze.is_wall(robot.v, robot.h, robot.direction, this_maze.v_maze))
         {
-            /*  if there's a wall turn left and try again */
+            /*  if there's a wall, turn left and try again */
             robot.direction = turn_left(robot);
             exit_search(robot, this_maze);
         }

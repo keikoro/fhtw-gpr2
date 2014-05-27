1253aee8a6968c54c1c9de62de59aed0e276232a
K Kollmann <git@k.kollmann.moe>
2014-05-27 17:31:33 +0200

    added most basic comments

--- labrob/mazes_dummyprint.cpp
+++ labrob/mazes_dummyprint.cpp
@@ -1,3 +1,14 @@
+/*
+    labrob program
+    mazes_findentrance.cpp
+
+    GPR2 UE5 + UE6 + UE7
+    if13b070 - K Kollmann
+    if13b076 - Linda Spindler
+
+
+*/
+
 using namespace std;
 
 
@@ -6,10 +17,7 @@ void Mazes::dummy_function_for_printing()
 {
 	cout << "Coordinates of the entrance: X, Y: " <<
 		entrance[0] << "," << entrance[1] << endl;
-
 	cout << "Coordinates of the exit: X, Y: " <<
 		mazeexit[0] << "," << mazeexit [1] << endl;
-		
 //	cout << "The robot enters the labyrinth facing: " << startposition << endl;
-
 }
